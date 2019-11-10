f90c94efa28495d9bafc16089627d39d5b736e68
Bartłomiej Dobosz <doboszb14@gmail.com>
2019-11-10 16:15:38 +0100

    Petla FOR

---
+++ Petla FOR.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-2">
+
+		<title>Instrukcja SWITCH...CASE</title>
+</head>
+<body>
+
+<table border=1>
+ 
+<?php
+
+for($i=1;$i<=20;$i+=5) {
+	echo $i." przebieg pętli<br>\n"."<br/>";
+}
+echo "<br/>";
+
+for($i=1;$i<=10;$i++) {
+	echo "<tr><td>$i</td>";
+	echo "<td>Przebieg pętli</td></tr>";
+}
+?>
+</table>
+</body>
+</html>
